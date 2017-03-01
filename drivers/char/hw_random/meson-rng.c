3e5ebf08c2bae52ec43e0d957f2ed9a5d85b57ef
Lawrence Mok <lawrence.mok@amlogic.com>
2017-02-28 16:09:18 -0800

    PD#152116: meson: rng: add option to limit rng data reading rate
    and added label and node name to common .dtsi
    
    To use 3us minimum between reads, add to the .dts:
    &rng {
            period = <3>;
    };
    
    Change-Id: I2391fa89d08994a402522434250e8689197f1759
    Signed-off-by: Lawrence Mok <lawrence.mok@amlogic.com>

--- drivers/char/hw_random/meson-rng.c
+++ drivers/char/hw_random/meson-rng.c
@@ -14,6 +14,7 @@
  */
 
 #include <linux/hw_random.h>
+#include <linux/delay.h>
 #include <linux/init.h>
 #include <linux/io.h>
 #include <linux/kernel.h>
@@ -25,11 +26,35 @@
 
 #define RNG_DATA 0x00
 
+/* minimum time between 32-bit RNG data reads in us */
+#define MESON_RNG_DEFAULT_PERIOD   (2)
+
+static u64 min_period = MESON_RNG_DEFAULT_PERIOD;
+static ktime_t expires;
+
 static int meson_read(struct hwrng *rng, void *buf, size_t max, bool wait)
 {
 	void __iomem *rng_base = (void __iomem *)rng->priv;
 
+	if (min_period > 0) {
+		while (1) {
+			s64 delta;
+			ktime_t now = ktime_get();
+			delta = ktime_us_delta(expires, now);
+			if (delta > 0 && delta <= min_period) {
+				if (!wait)
+					return 0;
+				udelay(delta);
+			} else {
+				break;
+			}
+		}
+	}
+
 	*(u32 *)buf = __raw_readl(rng_base + RNG_DATA);
+
+	if (min_period > 0)
+		expires = ktime_add_us(ktime_get(), min_period);
 	return sizeof(u32);
 }
 
@@ -43,7 +68,12 @@ static int meson_rng_probe(struct platform_device *pdev)
 	struct device *dev = &pdev->dev;
 	struct device_node *np = dev->of_node;
 	void __iomem *rng_base;
-	int err;
+	int err, i;
+
+	expires = ktime_get();
+
+	if (!of_property_read_u32(np, "period", &i))
+		min_period = i;
 
 	/* map peripheral */
 	rng_base = of_iomap(np, 0);
