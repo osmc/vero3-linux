70c71f54ccaab6ada0c3f9a2c8973bf7616fc3c2
Bo Yang <bo.yang@amlogic.com>
2017-08-10 14:24:51 +0800

    PD#148767: gxlx: pinctrl; add pinctrl support for gxlx
    Add new gpios support: GPIOH_10\11\12
    
    Change-Id: I2fe055def0dc0575ee099c07661564b165f554ff
    Signed-off-by: Bo Yang <bo.yang@amlogic.com>

---
+++ include/dt-bindings/gpio/gxlx.h
@@ -0,0 +1,133 @@
+#ifndef __ARCH_GXLX_GPIO_H__
+#define __ARCH_GXLX_GPIO_H__
+/*AO Bank*/
+#define	GPIOAO_0	0
+#define	GPIOAO_1	1
+#define	GPIOAO_2	2
+#define	GPIOAO_3	3
+#define	GPIOAO_4	4
+#define	GPIOAO_5	5
+#define	GPIOAO_6	6
+#define	GPIOAO_7	7
+#define	GPIOAO_8	8
+#define	GPIOAO_9	9
+
+/*EE Bank*/
+#define	GPIOZ_0		0
+#define	GPIOZ_1		1
+#define	GPIOZ_2		2
+#define	GPIOZ_3		3
+#define	GPIOZ_4		4
+#define	GPIOZ_5		5
+#define	GPIOZ_6		6
+#define	GPIOZ_7		7
+#define	GPIOZ_8		8
+#define	GPIOZ_9		9
+#define	GPIOZ_10	10
+#define	GPIOZ_11	11
+#define	GPIOZ_12	12
+#define	GPIOZ_13	13
+#define	GPIOZ_14	14
+#define	GPIOZ_15	15
+
+#define	GPIOH_0		16
+#define	GPIOH_1		17
+#define	GPIOH_2		18
+#define	GPIOH_3		19
+#define	GPIOH_4		20
+#define	GPIOH_5		21
+#define	GPIOH_6		22
+#define	GPIOH_7		23
+#define	GPIOH_8		24
+#define	GPIOH_9		25
+#define	GPIOH_10	26
+#define	GPIOH_11	27
+#define	GPIOH_12	28
+
+#define	BOOT_0		29
+#define	BOOT_1		30
+#define	BOOT_2		31
+#define	BOOT_3		32
+#define	BOOT_4		33
+#define	BOOT_5		34
+#define	BOOT_6		35
+#define	BOOT_7		36
+#define	BOOT_8		37
+#define	BOOT_9		38
+#define	BOOT_10		39
+#define	BOOT_11		40
+#define	BOOT_12		41
+#define	BOOT_13		42
+#define	BOOT_14		43
+#define	BOOT_15		44
+
+#define	CARD_0		45
+#define	CARD_1		46
+#define	CARD_2		47
+#define	CARD_3		48
+#define	CARD_4		49
+#define	CARD_5		50
+#define	CARD_6		51
+
+#define	GPIODV_0	52
+#define	GPIODV_1	53
+#define	GPIODV_2	54
+#define	GPIODV_3	55
+#define	GPIODV_4	56
+#define	GPIODV_5	57
+#define	GPIODV_6	58
+#define	GPIODV_7	59
+#define	GPIODV_8	60
+#define	GPIODV_9	61
+#define	GPIODV_10	62
+#define	GPIODV_11	63
+#define	GPIODV_12	64
+#define	GPIODV_13	65
+#define	GPIODV_14	66
+#define	GPIODV_15	67
+#define	GPIODV_16	68
+#define	GPIODV_17	69
+#define	GPIODV_18	70
+#define	GPIODV_19	71
+#define	GPIODV_20	72
+#define	GPIODV_21	73
+#define	GPIODV_22	74
+#define	GPIODV_23	75
+#define	GPIODV_24	76
+#define	GPIODV_25	77
+#define	GPIODV_26	78
+#define	GPIODV_27	79
+#define	GPIODV_28	80
+#define	GPIODV_29	81
+
+#define	GPIOX_0		82
+#define	GPIOX_1		83
+#define	GPIOX_2		84
+#define	GPIOX_3		85
+#define	GPIOX_4		86
+#define	GPIOX_5		87
+#define	GPIOX_6		88
+#define	GPIOX_7		89
+#define	GPIOX_8		90
+#define	GPIOX_9		91
+#define	GPIOX_10	92
+#define	GPIOX_11	93
+#define	GPIOX_12	94
+#define	GPIOX_13	95
+#define	GPIOX_14	96
+#define	GPIOX_15	97
+#define	GPIOX_16	98
+#define	GPIOX_17	99
+#define	GPIOX_18	100
+
+#define	GPIOCLK_0	101
+#define	GPIOCLK_1	102
+
+#define	GPIO_TEST_N	103
+
+
+/*AO REG */
+#define AO	0x10
+#define AO2	0x11
+
+#endif /* __ARCH_GXLX_GPIO_H__ */
