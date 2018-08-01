43e63d1c99c4f52b4ca8ccf5313c3b01fb222591
Sam Nazarko <email@samnazarko.co.uk>
2018-08-01 17:36:27 +0100

    Add OSMC OpenLinux 4.9 release
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

---
+++ drivers/amlogic/mailbox/meson_mhu.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: (GPL-2.0+ OR MIT) */
+/*
+ * Copyright (c) 2019 Amlogic, Inc. All rights reserved.
+ */
+
+#ifndef __MESON_MHU_H__
+#define __MESON_MHU_H__
+#include <linux/amlogic/meson_mhu_common.h>
+
+#define CONTROLLER_NAME		"mhu_ctlr"
+
+#define CHANNEL_MAX		2
+#define CHANNEL_LOW_PRIORITY	"cpu_to_scp_low"
+#define CHANNEL_HIGH_PRIORITY	"cpu_to_scp_high"
+
+extern u32 num_scp_chans;
+extern u32 send_listen_chans;
+#endif
