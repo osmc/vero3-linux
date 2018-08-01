1d1639ddad130f3e8e6c2b2fb1262b46a04d1ef6
Sam Nazarko <email@samnazarko.co.uk>
2018-08-01 17:36:27 +0100

    Add OSMC OpenLinux 4.9 release
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

---
+++ drivers/amlogic/media/dtv_demod/include/addr_atsc_cntr.h
@@ -0,0 +1,35 @@
+/*
+ * drivers/amlogic/media/dtv_demod/include/addr_atsc_cntr.h
+ *
+ * Copyright (C) 2017 Amlogic, Inc. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ */
+
+#ifndef __ADDR_ATSC_CNTR_H__
+#define __ADDR_ATSC_CNTR_H__
+
+#include "addr_atsc_cntr_bit.h"
+
+#define  ATSC_CNTR_REG_0X20                  (0x20)
+#define  ATSC_CNTR_REG_0X21                  (0x21)
+#define  ATSC_CNTR_REG_0X22                  (0x22)
+#define  ATSC_CNTR_REG_0X23                  (0x23)
+#define  ATSC_CNTR_REG_0X24                  (0x24)
+#define  ATSC_CNTR_REG_0X29                  (0x29)
+#define  ATSC_CNTR_REG_0X2A                  (0x2a)
+#define  ATSC_CNTR_REG_0X2B                  (0x2b)
+#define  ATSC_CNTR_REG_0X2C                  (0x2c)
+#define  ATSC_CNTR_REG_0X2D                  (0x2d)
+#define  ATSC_CNTR_REG_0X2E                  (0x2e)
+
+#endif
