6fd6346a536d1b2a75359707bedd7bb6ee2b83a8
Sam Nazarko <email@samnazarko.co.uk>
2018-09-29 05:00:40 +0100

    stmmac: fix coalecising frame pauses
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- drivers/net/ethernet/stmicro/stmmac/common.h
+++ drivers/net/ethernet/stmicro/stmmac/common.h
@@ -200,9 +200,9 @@ struct stmmac_extra_stats {
 #define MAX_DMA_RIWT		0xff
 #define MIN_DMA_RIWT		0x20
 /* Tx coalesce parameters */
-#define STMMAC_COAL_TX_TIMER	40000
+#define STMMAC_COAL_TX_TIMER	1000
 #define STMMAC_MAX_COAL_TX_TICK	100000
-#define STMMAC_TX_MAX_FRAMES	256
+#define STMMAC_TX_MAX_FRAMES	2
 #define STMMAC_TX_FRAMES	64
 
 /* Rx IPC status */
